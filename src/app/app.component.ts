1874a2264f0b9395587fb72e042fa98ffed03c9e
Soumik199 <Soumikroy519@gmail.com>
2022-02-24 19:21:21 +0530

    Form Creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'regform';
+
+  citylist=[ 'Durgapur','kolkata','Burdwan','Lucknow','Delhi'];
+  onSubmit(contactForm : NgForm){
+    console.log(contactForm.value);
+    alert("The Data is stored oin server")
+  }
 }
